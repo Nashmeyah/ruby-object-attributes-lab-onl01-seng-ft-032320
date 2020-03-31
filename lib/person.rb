907fff26a1da7d9baea43e2b5caf49ab18942595
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-31 03:13:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,13 +4,14 @@ class Person
     @job = name
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
+  
 
   def job
     @job
